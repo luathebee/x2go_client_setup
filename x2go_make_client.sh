a4d3e78fa7ab8cb492fd1a4eeae100efd481f4d5
Rubens Zanatta <rubenszanatta@hotmail.com>
2019-02-07 09:38:33 -0200

    script não remove mais o krb5.conf modelo da pasta

--- x2go_make_client.sh
+++ x2go_make_client.sh
@@ -11,21 +11,28 @@ fi
 echo "----------------------------------"
 echo "-----------A V I S O--------------"
 echo "----------------------------------"
-echo " Esse script irá sobre escrever o arquivo krb5.conf"
+echo " Esse script irá criar um backup e sobrescrever o arquivo krb5.conf"
 echo " Tem certeza que deseja continuar? [y]"
 read continuar
-if [continuar == "y"] 
+if [ $continuar = "y"] 
 	then echo "Finalizando"
 	exit
 fi
 
-
-
 #instala x2go client from ppa
-echo "Instlando X2Go"
+echo "Instalando X2Go"
 sudo apt-add-repository -y ppa:x2go/stable
 apt-get update
 apt-get install -y x2goclient 
-echo "Instalado gg ez"
 
-sudo apt-get install krb5-user
+
+## Instala e configurar Kerberos
+echo "Instalando Kerberos User"
+apt-get install krb5-user
+if [ -f /etc/krb5.conf ]; then
+    echo "Criando backup do krb5.conf existente"
+    cp /etc/krb5.conf krb5.conf.old
+fi
+cp krb5.conf /etc/krb5.conf
+
+
